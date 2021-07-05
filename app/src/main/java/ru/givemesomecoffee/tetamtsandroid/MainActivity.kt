5483a7b96896197a99368785fb4875a1f19edfd2
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-07-05 16:59:17 +0300

    Initial commit.

---
+++ app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.givemesomecoffee.tetamtsandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
