6c9de74f4aa7f26653f079a37756fddb015112d7
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-07-08 21:58:17 +0300

    added horizontal layout(activity_movie_details_h) and removed bg color in theme

--- app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
+++ app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.givemesomecoffee.tetamtsandroid
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        switchLayouts()
+    }
+
+    private fun switchLayouts(){
+        val config = resources.configuration
+        if(config.orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setContentView(R.layout.activity_movie_details_h)
+        } else {
+            setContentView(R.layout.activity_movie_details)
+        }
     }
 }
