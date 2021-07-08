3910cfbd708f4dfdd43b14baca1deda701933272
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-07-08 18:25:34 +0300

    cleaned unused imports, and renamed gray color to grey

--- app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
+++ app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
@@ -2,8 +2,6 @@ package ru.givemesomecoffee.tetamtsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
