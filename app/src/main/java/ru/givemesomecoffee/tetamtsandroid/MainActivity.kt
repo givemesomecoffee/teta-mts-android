9329b06a1d49246bb3e6f15ad95facf0a70163fb
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-07-08 16:40:03 +0300

    layout activity_movie_details added

--- app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
+++ app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.givemesomecoffee.tetamtsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_details)
     }
 }
