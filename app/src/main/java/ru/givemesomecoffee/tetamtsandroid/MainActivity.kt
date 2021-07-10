6a1b7e488f7bd63e2969b32a51f74973f193eb3e
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-07-10 22:56:56 +0300

    backup layout movie details

--- app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
+++ app/src/main/java/ru/givemesomecoffee/tetamtsandroid/MainActivity.kt
@@ -1,12 +1,11 @@
 package ru.givemesomecoffee.tetamtsandroid
 
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_movie_details)
+        setContentView(R.layout.activity_movie_list)
     }
 }
