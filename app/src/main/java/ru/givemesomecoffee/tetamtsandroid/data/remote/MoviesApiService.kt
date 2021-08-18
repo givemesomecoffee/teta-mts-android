6b9823a4994ddb523d1889a06094fc78cc11991d
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-08-18 21:40:40 +0300

    moved repeated query(api_ley & lang) to interceptor

--- app/src/main/java/ru/givemesomecoffee/tetamtsandroid/data/remote/MoviesApiService.kt
+++ app/src/main/java/ru/givemesomecoffee/tetamtsandroid/data/remote/MoviesApiService.kt
@@ -2,6 +2,8 @@ package ru.givemesomecoffee.tetamtsandroid.data.remote
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -12,40 +14,51 @@ import ru.givemesomecoffee.tetamtsandroid.data.remote.entity.GenresResponse
 import ru.givemesomecoffee.tetamtsandroid.data.remote.entity.MovieApiResponse
 import ru.givemesomecoffee.tetamtsandroid.data.remote.entity.MoviesApiResponse
 
+const val API_BASE_URL = "https://api.themoviedb.org/3/"
+const val API_LANG_QUERY = "ru-RU"
+const val API_TOKEN = "0b60005e258e5e6a053da6f4870cf6bf"
+const val API_POPULARITY_SORT_BY = "popularity.desc"
+
 interface MoviesApiService {
     @GET("discover/movie")
     suspend fun getMovies(
-        @Query("api_key") apiKey: String = "0b60005e258e5e6a053da6f4870cf6bf",
-        @Query("sort_by") endpoint: String = "popularity.desc",
-        @Query("language") lang: String = "ru-RU"
+        @Query("sort_by") endpoint: String = API_POPULARITY_SORT_BY
     ): MoviesApiResponse
 
     @GET("discover/movie")
     suspend fun getMoviesByGenre(
-        @Query("api_key") apiKey: String = "0b60005e258e5e6a053da6f4870cf6bf",
-        @Query("sort_by") endpoint: String = "popularity.desc",
-        @Query("language") lang: String = "ru-RU",
+        @Query("sort_by") endpoint: String = API_POPULARITY_SORT_BY,
         @Query("with_genres") genre: String
     ): MoviesApiResponse
 
     @GET("movie/{movie_id}")
     suspend fun getMovie(
         @Path("movie_id") id: String,
-        @Query("api_key") apiKey: String = "0b60005e258e5e6a053da6f4870cf6bf",
-        @Query("language") lang: String = "ru-RU",
         @Query("append_to_response") type: String = "credits"
     ): MovieApiResponse
 
     @GET("genre/movie/list")
-    suspend fun getGenres(@Query("api_key") apiKey: String = "0b60005e258e5e6a053da6f4870cf6bf",  @Query("language") lang: String = "ru-RU"): GenresResponse
+    suspend fun getGenres(): GenresResponse
 
     @GET("movie/{id}/release_dates")
-    suspend fun getCert(
-        @Path("id") id: String,
-        @Query("api_key") apiKey: String = "0b60005e258e5e6a053da6f4870cf6bf"
-    ): CertificationResponse
+    suspend fun getCert(@Path("id") id: String): CertificationResponse
 
     companion object {
+        private fun getOkHttpClient(): OkHttpClient {
+            val interceptor = Interceptor { chain ->
+                val url = chain.request().url().newBuilder()
+                    .addQueryParameter("api_key", API_TOKEN)
+                    .addQueryParameter("language", API_LANG_QUERY)
+                    .build()
+                val request = chain.request().newBuilder()
+                    .url(url)
+                    .build()
+                chain.proceed(request)
+            }
+
+            return OkHttpClient.Builder().addInterceptor(interceptor).build()
+        }
+
         fun create(): MoviesApiService {
             val moshi = Moshi.Builder()
                 .add(KotlinJsonAdapterFactory())
@@ -53,7 +66,8 @@ interface MoviesApiService {
 
             return Retrofit.Builder()
                 .addConverterFactory(MoshiConverterFactory.create(moshi))
-                .baseUrl("https://api.themoviedb.org/3/")
+                .client(getOkHttpClient())
+                .baseUrl(API_BASE_URL)
                 .build()
                 .create(MoviesApiService::class.java)
         }
