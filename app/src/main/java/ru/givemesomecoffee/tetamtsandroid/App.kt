48056135ac1aada17427aa2a795bb81cc2c58dc7
givemesomecoffee <givemesomecoffee@yandex.ru>
2021-08-22 22:20:32 +0300

    added notification for work manager job done

--- app/src/main/java/ru/givemesomecoffee/tetamtsandroid/App.kt
+++ app/src/main/java/ru/givemesomecoffee/tetamtsandroid/App.kt
@@ -1,6 +1,11 @@
 package ru.givemesomecoffee.tetamtsandroid
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.util.Log
 import androidx.work.*
 import ru.givemesomecoffee.data.DaggerDataComponent
 import ru.givemesomecoffee.data.DataComponent
@@ -10,9 +15,14 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class App : Application() {
+    private val constraints = Constraints.Builder()
+        .setRequiredNetworkType(NetworkType.CONNECTED)
+        .build()
+
     private val workRequest: PeriodicWorkRequest = PeriodicWorkRequestBuilder<RefreshDataService>(
         20, TimeUnit.MINUTES
     ) // flexInterval
+        .setConstraints(constraints)
         .build()
 
     @Inject
@@ -25,9 +35,26 @@ class App : Application() {
 
         WorkManager.initialize(this, config)
     }
-
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "channel name"
+            val descriptionText = "channel desc"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+            Log.d("test", notificationManager.notificationChannels.toString())
+        }
+    }
     override fun onCreate() {
         super.onCreate()
+        createNotificationChannel()
 
         dataComponent = DaggerDataComponent.builder()
             .application(this)
--- app/src/main/java/ru/givemesomecoffee/tetamtsandroid/RefreshDataService.kt
+++ app/src/main/java/ru/givemesomecoffee/tetamtsandroid/RefreshDataService.kt
@@ -1,7 +1,11 @@
 package ru.givemesomecoffee.tetamtsandroid
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.work.ListenableWorker
 import androidx.work.Worker
 import androidx.work.WorkerFactory
@@ -10,29 +14,63 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import ru.givemesomecoffee.tetamtsandroid.domain.cases.MoviesListCases
+import ru.givemesomecoffee.tetamtsandroid.presentation.MainActivity
 import javax.inject.Inject
 
-class RefreshDataService @Inject constructor(context: Context, workerParams: WorkerParameters) :
-    Worker(context, workerParams){
+const val CHANNEL_ID = "123"
+
+class RefreshDataService @Inject constructor(val context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
     lateinit var domain: MoviesListCases
     override fun doWork(): Result {
+
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        var builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_search)
+            .setContentTitle("test")
+            .setContentText("test text")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setAutoCancel(true)
+
+
+
         Log.d("workService", "im running")
         CoroutineScope(Dispatchers.IO).launch {
             domain.getMoviesList(null)
             Log.d("workService", "job finished")
         }
 
+        with(NotificationManagerCompat.from(context)) {
+            Log.d("workService", "job finished2")
+            // notificationId is a unique int for each notification that you must define
+            notify(123, builder.build())
+        }
+        Log.d("workService", "job finished2")
         return Result.success()
     }
 
+
 }
 
-class DaggerWorkerFactory @Inject constructor(private val domain: MoviesListCases) : WorkerFactory() {
+class DaggerWorkerFactory @Inject constructor(private val domain: MoviesListCases) :
+    WorkerFactory() {
 
-    override fun createWorker(appContext: Context, workerClassName: String, workerParameters: WorkerParameters): ListenableWorker? {
+    override fun createWorker(
+        appContext: Context,
+        workerClassName: String,
+        workerParameters: WorkerParameters
+    ): ListenableWorker? {
 
         val workerKlass = Class.forName(workerClassName).asSubclass(Worker::class.java)
-        val constructor = workerKlass.getDeclaredConstructor(Context::class.java, WorkerParameters::class.java)
+        val constructor =
+            workerKlass.getDeclaredConstructor(Context::class.java, WorkerParameters::class.java)
         val instance = constructor.newInstance(appContext, workerParameters)
 
         when (instance) {
